e6826b5d19c72461c3ef99e1a2df38474e62a12b
nandan chauhan <nandanc@clavax.com>
2022-03-20 11:36:09 +0530

    counter app updated

--- counter/src/index.js
+++ counter/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 
-//const myfirstelement = <h1>Hello React!</h1>
 
 ReactDOM.render(<App />, document.getElementById('root'));
